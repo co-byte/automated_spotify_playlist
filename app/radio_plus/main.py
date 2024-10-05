531619c7d6e31012a6aea62d0930e446d217aaa5
CobeVdS <cobe.vandersteen@ugent.be>
2024-10-05 23:05:06 +0200

    creates an initial project file structure

---
+++ app/radio_plus/main.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from app.spotify.spotify_module import SpotifyAPI
+from radio_plus_module import RadioPlusAPI
+
+import temp_secrets
+
+client_id = temp_secrets.client_id
+client_secret = temp_secrets.client_secret
+redirect_uri = "https://github.com/co-byte?tab=repositories"
+user_id = temp_secrets.user_id
+date_val = datetime.strftime(datetime.now(),"%Y-%m-%d")
+time_val = datetime.strftime(datetime.now(),"%H:%M:%S") 
+selected_playlist_name = "Todays generated playlist"
+
+
+def update_playlist():
+    spotify = SpotifyAPI(client_id=client_id,
+                         client_secret=client_secret,
+                         redirect_uri=redirect_uri
+                         )
+    selected_playlist_id = spotify.get_playlist_id(playlist_name=selected_playlist_name,
+                                                   user_id=user_id
+                                                   )
+
+    station_name = spotify.get_radio_station_from_playlist_description(selected_playlist_id)
+
+    station_id = RadioPlusAPI.turn_station_name_into_id(station_name)
+    RadioPlusAPI.get_daily_playlists(station_id=station_id, date=0)
+
+    todays_query = RadioPlusAPI.export_daily_playlist(station_id=station_id)
+    todays_tracks_ids = spotify.get_query_track_ids(query_list=todays_query)
+    spotify.generate_playlist_from_query(track_ids=todays_tracks_ids,
+                                                  playlist_name=selected_playlist_name,
+                                                  is_public=True,
+                                                  description=f"Queued station: {station_name} - Latest update: {date_val} @ {time_val} (GMT+2)",
+                                                  is_collaborative=False)
+    print(f"{todays_query=}")
+    return todays_query
+
+
+if __name__ == "__main__":
+    played_tracks = update_playlist()
+
+    tryCount = 0
+    while(tryCount < 3 and not played_tracks):
+        played_tracks = update_playlist
+        tryCount+=1
